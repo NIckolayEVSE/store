30534dc41f621e05ff0bd25376958f2a7cc18a7e
NIckolayEVSE <biomaster2014@yandex.ru>
2023-04-30 20:01:18 +0300

    new

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from products.models import ProductCategory, Product
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
