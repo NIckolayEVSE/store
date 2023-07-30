667ce704366ab6f060f7e442a70c911cdd3cdc4e
NIckolayEVSE <biomaster2014@yandex.ru>
2023-07-30 13:31:52 +0300

    правки

--- products/models.py
+++ products/models.py
@@ -5,14 +5,39 @@ from django.db import models
 
 
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    description = models.TextField(null=True, blank=True)
+    name = models.CharField(
+        max_length=128,
+        unique=True,
+        verbose_name='Название категории товаров'
+    )
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name='Описание категории'
+    )
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=20, decimal_places=2)
-    quantity = models.PositiveBigIntegerField(default=0)
-    image = models.ImageField(upload_to='products_images')
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField(
+        max_length=256
+    )
+    description = models.TextField(
+        verbose_name='Описание товара'
+    )
+    price = models.DecimalField(
+        max_digits=20,
+        decimal_places=2,
+        verbose_name='Цена товара'
+    )
+    quantity = models.PositiveBigIntegerField(
+        default=0,
+        verbose_name='Количество товара'
+    )
+    image = models.ImageField(
+        upload_to='products_images'
+    )
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.CASCADE,
+        related_name='category'
+    )
