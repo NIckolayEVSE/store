b16a583c84b158ca3f4faff2d365c0b07fc47351
NIckolayEVSE <biomaster2014@yandex.ru>
2023-08-20 13:25:41 +0300

    правки

--- products/views.py
+++ products/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import HttpResponseRedirect
 from django.views.generic import TemplateView, ListView
 
 from products.models import Product, ProductCategory, Basket
-
-from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 
 
 # Create your views here.
